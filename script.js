857f5f480a5fe8a80d5030b9c3063650ef30228c
amandaluise14 <amanda_l_maia@estudante.sesisenai.org.br>
2026-03-16 09:17:43 -0300

    add - consegui realizar a estrutura da calculadora e faze-la funcionar

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 function calcular(){
 
-    
+    let n1 = Number(document.getElementById("n1").value);
+    let n2 = Number(document.getElementById("n2").value);
+
+    let opcoes = document.getElementById("opcoes").value;
+
+
+    let resultado;
+
+    if(opcoes == "1"){
+        resultado = n1+n2;
+    }else if(opcoes == "2"){
+        resultado = n1-n2;
+    }else if(opcoes == "3" ){
+        resultado = n1*n2
+    }else if(opcoes == "4"){
+        resultado = n1/n2
+    }
+
+    document.getElementById("Resultado").innerHTML = resultado;
+
+
 }
